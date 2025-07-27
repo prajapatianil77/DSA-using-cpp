106bf8f0c968659d6803958719c2abfd63c23911
Anil Prajapat <163278629+prajapatianil77@users.noreply.github.com>
2025-07-27 07:33:01 +0530

    Update arrayPointer.cpp

--- arrayPointer.cpp
+++ arrayPointer.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int arr[] = {1,3,5,7};        // in cpp array is a pointer it store the address of first element of arr.
 
-    cout<<arr <<endl;    // arraw pointer is const type we can't change it 
+    cout<<arr <<endl;    // array pointer is const type we can't change it 
 
-    cout <<*arr;    // give the firdt index value
+    cout <<*arr;    // give the first index value
 }
